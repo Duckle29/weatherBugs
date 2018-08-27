5c553ae0af6870d4732d20546a1e7c64efae8c1a
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2018-08-27 21:56:42 +0200

    Switched to PIO lib manager, and added credentials template

--- weatherStation/src/credentials.template.h
+++ weatherStation/src/credentials.template.h
@@ -4,4 +4,4 @@ const char* ssid = "Your SSID here";
 const char* password = "Your Wifi Password Here";
 
 const char* WG_ID = "Your Weather Station ID here";
-const char* WG_PASS = "Your Weather Station pass here";
+const char* WG_PASS = "Your Weather Station pass here";s
